acf8a37ee205607a68d29ea144c56584c9db9b4a
Toze Cerdeira <tozecerdeira@hotmail.com>
2024-08-05 19:15:37 +0100

    Add project files.

---
+++ OCR Toze/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace OCR_Toze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonIronOCR = new System.Windows.Forms.Button();
+            this.buttonBlack = new System.Windows.Forms.Button();
+            this.buttonGray = new System.Windows.Forms.Button();
+            this.buttonOriginal = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.buttonOpenImage = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.buttonTesseract2 = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonClearImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(3, 15);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Abrir";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 287);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(402, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(367, 352);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonIronOCR
+            // 
+            this.buttonIronOCR.Location = new System.Drawing.Point(403, 64);
+            this.buttonIronOCR.Name = "buttonIronOCR";
+            this.buttonIronOCR.Size = new System.Drawing.Size(75, 23);
+            this.buttonIronOCR.TabIndex = 3;
+            this.buttonIronOCR.Text = "Iron OCR";
+            this.buttonIronOCR.UseVisualStyleBackColor = true;
+            this.buttonIronOCR.Click += new System.EventHandler(this.buttonIronOCR_Click);
+            // 
+            // buttonBlack
+            // 
+            this.buttonBlack.Location = new System.Drawing.Point(24, 8);
+            this.buttonBlack.Name = "buttonBlack";
+            this.buttonBlack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBlack.TabIndex = 5;
+            this.buttonBlack.Text = "Black";
+            this.buttonBlack.UseVisualStyleBackColor = true;
+            this.buttonBlack.Click += new System.EventHandler(this.buttonBlack_Click);
+            // 
+            // buttonGray
+            // 
+            this.buttonGray.Location = new System.Drawing.Point(105, 8);
+            this.buttonGray.Name = "buttonGray";
+            this.buttonGray.Size = new System.Drawing.Size(75, 23);
+            this.buttonGray.TabIndex = 6;
+            this.buttonGray.Text = "Gray";
+            this.buttonGray.UseVisualStyleBackColor = true;
+            this.buttonGray.Click += new System.EventHandler(this.buttonGray_Click);
+            // 
+            // buttonOriginal
+            // 
+            this.buttonOriginal.Location = new System.Drawing.Point(267, 8);
+            this.buttonOriginal.Name = "buttonOriginal";
+            this.buttonOriginal.Size = new System.Drawing.Size(75, 23);
+            this.buttonOriginal.TabIndex = 7;
+            this.buttonOriginal.Text = "Original";
+            this.buttonOriginal.UseVisualStyleBackColor = true;
+            this.buttonOriginal.Click += new System.EventHandler(this.buttonOriginal_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox2.Location = new System.Drawing.Point(324, 47);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(317, 287);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(186, 8);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(75, 23);
+            this.buttonColor.TabIndex = 9;
+            this.buttonColor.Text = "Color";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.buttonColor);
+            this.panel1.Controls.Add(this.buttonBlack);
+            this.panel1.Controls.Add(this.buttonGray);
+            this.panel1.Controls.Add(this.buttonOriginal);
+            this.panel1.Location = new System.Drawing.Point(6, 14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(367, 40);
+            this.panel1.TabIndex = 10;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.salvarToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(106, 48);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // salvarToolStripMenuItem
+            // 
+            this.salvarToolStripMenuItem.Name = "salvarToolStripMenuItem";
+            this.salvarToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.salvarToolStripMenuItem.Text = "Salvar";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 468);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 62);
+            this.panel2.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(543, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 42);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Image Convert";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(15, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Text Editor";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Controls.Add(this.buttonOpen);
+            this.panel3.Location = new System.Drawing.Point(9, 9);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(677, 447);
+            this.panel3.TabIndex = 12;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel4.Controls.Add(this.buttonClearImage);
+            this.panel4.Controls.Add(this.buttonClear);
+            this.panel4.Controls.Add(this.buttonTesseract2);
+            this.panel4.Controls.Add(this.pictureBox3);
+            this.panel4.Controls.Add(this.buttonOpenImage);
+            this.panel4.Controls.Add(this.buttonIronOCR);
+            this.panel4.Controls.Add(this.panel1);
+            this.panel4.Controls.Add(this.richTextBox1);
+            this.panel4.Location = new System.Drawing.Point(9, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(794, 447);
+            this.panel4.TabIndex = 13;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // buttonOpenImage
+            // 
+            this.buttonOpenImage.Location = new System.Drawing.Point(6, 60);
+            this.buttonOpenImage.Name = "buttonOpenImage";
+            this.buttonOpenImage.Size = new System.Drawing.Size(125, 23);
+            this.buttonOpenImage.TabIndex = 10;
+            this.buttonOpenImage.Text = "Open Image";
+            this.buttonOpenImage.UseVisualStyleBackColor = true;
+            this.buttonOpenImage.Click += new System.EventHandler(this.buttonOpenImage_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox3.Location = new System.Drawing.Point(6, 92);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(367, 349);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // buttonTesseract2
+            // 
+            this.buttonTesseract2.Location = new System.Drawing.Point(484, 63);
+            this.buttonTesseract2.Name = "buttonTesseract2";
+            this.buttonTesseract2.Size = new System.Drawing.Size(75, 23);
+            this.buttonTesseract2.TabIndex = 12;
+            this.buttonTesseract2.Text = "Tesseract";
+            this.buttonTesseract2.UseVisualStyleBackColor = true;
+            this.buttonTesseract2.Click += new System.EventHandler(this.buttonTesseract2_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClear.Location = new System.Drawing.Point(694, 63);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 13;
+            this.buttonClear.Text = "Clear text";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonClearImage
+            // 
+            this.buttonClearImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClearImage.Location = new System.Drawing.Point(298, 64);
+            this.buttonClearImage.Name = "buttonClearImage";
+            this.buttonClearImage.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearImage.TabIndex = 14;
+            this.buttonClearImage.Text = "Clear Image";
+            this.buttonClearImage.UseVisualStyleBackColor = true;
+            this.buttonClearImage.Click += new System.EventHandler(this.buttonClearImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "OCR Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonIronOCR;
+        private System.Windows.Forms.Button buttonBlack;
+        private System.Windows.Forms.Button buttonGray;
+        private System.Windows.Forms.Button buttonOriginal;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonOpenImage;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button buttonTesseract2;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonClearImage;
+    }
+}
+
---
+++ OCR Toze/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IronOcr;
+using IronOcr.Languages;
+using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+using Tesseract;
+
+namespace OCR_Toze
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _imagem;
+        private string _path;
+        private Bitmap currentImage;
+        private Bitmap originalImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel3.Visible = false;
+            panel4.Visible = true;
+        }
+        private void buttonOpenImage_Click(object sender, EventArgs e)
+        {
+                        using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Set the filter for file types
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+                // Show the dialog and check if the user selected a file
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _path = openFileDialog.FileName;
+                    originalImage = new Bitmap(openFileDialog.FileName);
+                    currentImage = originalImage;
+                    // Load the selected image into the PictureBox
+                    pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox3.Image = originalImage;
+                }
+            }
+        }
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonIronOCR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            var ocr = new AutoOcr();           
+            var texto = ocr.Read(originalImage);
+            richTextBox1.Text = texto.ToString();
+            }
+            catch 
+            {
+                return;
+            }
+
+        }
+    
+        private void buttonTesserac_Click(object sender, EventArgs e)
+        {
+
+            // Load the OCR engine
+            using (var ocr = OcrApi.Create())
+            {
+                // Specify the language you want to use
+                ocr.Init(Languages.Portuguese);
+
+                // Load the image
+                using (var image = new Bitmap(_path))
+                {
+                    // Perform OCR on the image
+                    string text = ocr.GetTextFromImage(image);
+                    richTextBox1.Text = text;
+                }
+            }
+        }
+
+        private void buttonBlack_Click(object sender, EventArgs e)
+        {
+            // Convert to Black and White
+            currentImage = (Bitmap)originalImage.Clone();
+            for (int y = 0; y < currentImage.Height; y++)
+            {
+                for (int x = 0; x < currentImage.Width; x++)
+                {
+                    Color pixelColor = currentImage.GetPixel(x, y);
+                    Color newColor = pixelColor.GetBrightness() > 0.5 ? Color.White : Color.Black;
+                    currentImage.SetPixel(x, y, newColor);
+                }
+            }
+
+            pictureBox3.Image = currentImage;
+        }
+
+        private void buttonGray_Click(object sender, EventArgs e)
+        {
+            // Convert to Grayscale
+            currentImage = (Bitmap)originalImage.Clone();
+            for (int y = 0; y < currentImage.Height; y++)
+            {
+                for (int x = 0; x < currentImage.Width; x++)
+                {
+                    Color pixelColor = currentImage.GetPixel(x, y);
+                    int grayScale = (int)((pixelColor.R * 0.3) + (pixelColor.G * 0.59) + (pixelColor.B * 0.11));
+                    Color newColor = Color.FromArgb(grayScale, grayScale, grayScale);
+                    currentImage.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox3.Image = currentImage;
+        }
+
+        private void buttonOriginal_Click(object sender, EventArgs e)
+        {
+            // Reset to Initial Image
+            currentImage = (Bitmap)originalImage.Clone();
+            pictureBox3.Image = currentImage;
+        }
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            // Convert back to Color (Original Image)
+            currentImage = (Bitmap)originalImage.Clone();
+            pictureBox3.Image = currentImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+          
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private string TesseractOCR(string imagePath)
+        {
+            string resultText = string.Empty;
+            try
+            {
+                using (var _engine = new TesseractEngine(@"C:\Users\tozec\source\repos\OCR Toze\OCR Toze\tessdata\", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(imagePath))
+                    {
+                        using (var page = _engine.Process(img))
+                        {
+                            resultText = page.GetText();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error during OCR: " + ex.Message);
+            }
+            return resultText;
+        }
+
+        private void buttonTesseract2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string _text = TesseractOCR(_path);
+                richTextBox1.Text = _text;
+            }
+            catch { return; }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void buttonClearImage_Click(object sender, EventArgs e)
+        {
+            if(originalImage!=null)
+            {
+                originalImage.Dispose();
+                currentImage.Dispose();
+                pictureBox3.Image = null;
+            }
+            else { return; }
+
+        }
+    }
+
+}
